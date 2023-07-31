7a6cbd2f620e0f224636e4b441d2b90c1f135982
hsj3925 <hsj3925@naver.com>
2023-07-31 14:47:36 +0900

    0731

--- Server/Server.py
+++ Server/Server.py
@@ -3,93 +3,104 @@ import struct # 바이트(bytes) 형식의 데이터 처리 모듈
 import pickle # 객체의 직렬화 및 역직렬화 지원 모듈
 import cv2 # OpenCV(실시간 이미지 프로세싱) 모듈
 import pymysql
+from datetime import datetime
 
-conn = pymysql.connect(host='127.0.0.1', user='Admin', password='1592', db='k-hackaton_11', charset='utf8')
-cur = conn.cursor()
+class Server:
+	conn = None
+	cur = None
+	def __init__(self) -> None:
+		Server.conn = pymysql.connect(host='127.0.0.1', user='Admin', password='1592', db='k-hackaton_11', charset='utf8')
+		Server.cur = Server.conn.cursor()
 
-def Record(CCTV_ID: int, Crackdown_Type: int, Date: str, Locatioin_Id: int):
-	cur.execute(f'insert into crackdown(CCTV_ID, Location_ID, Date, Crackdown_Type) values(\'{CCTV_ID}\', \'{Locatioin_Id}\', \'{Date}\', \'{Crackdown_Type}\')')
+	def run(self):
+		# 서버 ip 주소 및 port 번호
+		ip = 'localhost'
+		port = 50001
 
-Record(1, 1, '20200125', 4)
-conn.commit()
+		# 소켓 객체 생성
+		server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+		# 소켓 주소 정보 할당
+		server_socket.bind((ip, port))
 
-# 서버 ip 주소 및 port 번호
-ip = 'localhost'
-port = 50001
+		# 연결 리스닝(동시 접속) 수 설정
+		server_socket.listen(10) 
 
-# 소켓 객체 생성
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		print('클라이언트 연결 대기')
 
-# 소켓 주소 정보 할당
-server_socket.bind((ip, port))
+		# 연결 수락(클라이언트 (소켓, 주소 정보) 반환)
+		client_socket, address = server_socket.accept()
+		print('클라이언트 ip 주소 :', address[0])
 
-# 연결 리스닝(동시 접속) 수 설정
-server_socket.listen(10) 
+		# 수신한 데이터를 넣을 버퍼(바이트 객체)
+		data_buffer = b""
 
-print('클라이언트 연결 대기')
+		# calcsize : 데이터의 크기(byte)
+		# - L : 부호없는 긴 정수(unsigned long) 4 bytes
+		data_size = struct.calcsize("L")
 
-# 연결 수락(클라이언트 (소켓, 주소 정보) 반환)
-client_socket, address = server_socket.accept()
-print('클라이언트 ip 주소 :', address[0])
+		while True:
+			# 설정한 데이터의 크기보다 버퍼에 저장된 데이터의 크기가 작은 경우
+			while len(data_buffer) < data_size:
+				# 데이터 수신
+				data_buffer += client_socket.recv(4096)
 
-# 수신한 데이터를 넣을 버퍼(바이트 객체)
-data_buffer = b""
+			# 버퍼의 저장된 데이터 분할
+			packed_data_size = data_buffer[:data_size]
+			data_buffer = data_buffer[data_size:] 
+			
+			# struct.unpack : 변환된 바이트 객체를 원래의 데이터로 반환
+			# - > : 빅 엔디안(big endian)
+			#   - 엔디안(endian) : 컴퓨터의 메모리와 같은 1차원의 공간에 여러 개의 연속된 대상을 배열하는 방법
+			#   - 빅 엔디안(big endian) : 최상위 바이트부터 차례대로 저장
+			# - L : 부호없는 긴 정수(unsigned long) 4 bytes 
+			frame_size = struct.unpack(">L", packed_data_size)[0]
+			
+			# 프레임 데이터의 크기보다 버퍼에 저장된 데이터의 크기가 작은 경우
+			while len(data_buffer) < frame_size:
+				# 데이터 수신
+				data_buffer += client_socket.recv(4096)
+			
+			# 프레임 데이터 분할
+			frame_data = data_buffer[:frame_size]
+			data_buffer = data_buffer[frame_size:]
+			
+			print("수신 프레임 크기 : {} bytes".format(frame_size))
+			
+			# loads : 직렬화된 데이터를 역직렬화
+			# - 역직렬화(de-serialization) : 직렬화된 파일이나 바이트 객체를 원래의 데이터로 복원하는 것
+			frame = pickle.loads(frame_data)
+			
+			# imdecode : 이미지(프레임) 디코딩
+			# 1) 인코딩된 이미지 배열
+			# 2) 이미지 파일을 읽을 때의 옵션
+			#    - IMREAD_COLOR : 이미지를 COLOR로 읽음
+			frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+			
+			# 프레임 출력
+			cv2.imshow('Frame', frame)
+			
+			# 'q' 키를 입력하면 종료
+			key = cv2.waitKey(1) & 0xFF
+			if key == ord("q"):
+				break
 
-# calcsize : 데이터의 크기(byte)
-# - L : 부호없는 긴 정수(unsigned long) 4 bytes
-data_size = struct.calcsize("L")
+		# 소켓 닫기
+		client_socket.close()
+		server_socket.close()
+		print('연결 종료')
 
-while True:
-	# 설정한 데이터의 크기보다 버퍼에 저장된 데이터의 크기가 작은 경우
-	while len(data_buffer) < data_size:
-		# 데이터 수신
-		data_buffer += client_socket.recv(4096)
-
-	# 버퍼의 저장된 데이터 분할
-	packed_data_size = data_buffer[:data_size]
-	data_buffer = data_buffer[data_size:] 
+		# 모든 창 닫기
+		cv2.destroyAllWindows()
 	
-	# struct.unpack : 변환된 바이트 객체를 원래의 데이터로 반환
-	# - > : 빅 엔디안(big endian)
-	#   - 엔디안(endian) : 컴퓨터의 메모리와 같은 1차원의 공간에 여러 개의 연속된 대상을 배열하는 방법
-	#   - 빅 엔디안(big endian) : 최상위 바이트부터 차례대로 저장
-	# - L : 부호없는 긴 정수(unsigned long) 4 bytes 
-	frame_size = struct.unpack(">L", packed_data_size)[0]
-	
-	# 프레임 데이터의 크기보다 버퍼에 저장된 데이터의 크기가 작은 경우
-	while len(data_buffer) < frame_size:
-		# 데이터 수신
-		data_buffer += client_socket.recv(4096)
-	
-	# 프레임 데이터 분할
-	frame_data = data_buffer[:frame_size]
-	data_buffer = data_buffer[frame_size:]
-	
-	print("수신 프레임 크기 : {} bytes".format(frame_size))
-	
-	# loads : 직렬화된 데이터를 역직렬화
-	# - 역직렬화(de-serialization) : 직렬화된 파일이나 바이트 객체를 원래의 데이터로 복원하는 것
-	frame = pickle.loads(frame_data)
-	
-	# imdecode : 이미지(프레임) 디코딩
-	# 1) 인코딩된 이미지 배열
-	# 2) 이미지 파일을 읽을 때의 옵션
-	#    - IMREAD_COLOR : 이미지를 COLOR로 읽음
-	frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-	
-	# 프레임 출력
-	cv2.imshow('Frame', frame)
-	
-	# 'q' 키를 입력하면 종료
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
+	def stop():
+		Server.conn.close()
 
-# 소켓 닫기
-client_socket.close()
-server_socket.close()
-print('연결 종료')
+	def Record(self, CCTV_ID: int, Crackdown_Type: int, Locatioin_Id: int):
+		Server.cur.execute(f'insert into crackdown(CCTV_ID, Location_ID, Date, Crackdown_Type) values(\'{CCTV_ID}\', \'{Locatioin_Id}\', \'{datetime.now()}\', \'{Crackdown_Type}\')')
+		Server.conn.commit()
 
-# 모든 창 닫기
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+	sv = Server()
+	sv.Record(1, 0, 0)
+	sv.run()
