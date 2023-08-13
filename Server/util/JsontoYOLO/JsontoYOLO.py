2109f862da750adc35d327533dc9c997f23d3f5a
hsj3925 <hsj3925@naver.com>
2023-08-13 22:41:27 +0900

    Update JsontoYOLO.py

--- Server/util/JsontoYOLO/JsontoYOLO.py
+++ Server/util/JsontoYOLO/JsontoYOLO.py
@@ -2,7 +2,7 @@ import json
 import os
 
 def pascal_voc_to_yolo(x1, y1, x2, y2, image_w, image_h):
-    return [((x2 + x1)/(2*image_w)), ((y2 + y1)/(2*image_h)), (x2 - x1)/image_w, (y2 - y1)/image_h]
+	return [((x2 + x1)/(2*image_w)), ((y2 + y1)/(2*image_h)), (x2 - x1)/image_w, (y2 - y1)/image_h]
 
 def createGt():
 	# Separate dataset - train, validation, test
